e327e808f16693dc6dd7b98733af4a228081c903
21DP2KSnik <106649035+KKsnikere@users.noreply.github.com>
2023-03-29 08:48:00 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,25 +1,50 @@
 import telebot
+from telebot.types import ReplyKeyboardMarkup, KeyboardButton
+
 bot = telebot.TeleBot('5978521960:AAFGhXFNOTDpMUmn-J3nYSZ_7_TpXLJnz6c')
+
+
+keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
+
+
+button_bmi = KeyboardButton('BMI')
+button_about = KeyboardButton('About')
+
+
+keyboard.add(button_bmi, button_about)
+
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, 'Hello')
+    bot.send_message(message.chat.id, 'Hello! Press the button to calculate BMI.', reply_markup=keyboard)
+
 
 @bot.message_handler(commands=['about'])
 def about(message):
-    about_msg="This Telegram bot is designed to help people stay fit and healthy by providing personalized workout plans and dietary advice.\nThe bot has several features, including a body mass index (BMI) calculator, also generates workout plans tailored to each user's fitness level, preferences, and goals, and recommends daily calorie intake based on their activity level, weight, and desired weight. It also offers helpful tips and advice on staying motivated and achieving fitness goals. With this bot, users can get all the support and guidance they need to lead a healthy lifestyle and stay in shape."
+    about_msg = "This is a Telegram bot designed to help people stay fit and healthy by providing personalized workout plans and dietary advice."
     bot.send_message(message.chat.id, about_msg)
 
-@bot.message_handler(commands=['help'])
-def bmi(message):
-    markup=types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
-    bmi = types.KeyboardButton('BMI')
-
-    markup.add(bmi)
-    bot.send_message(message.chat.id, 'Help', reply_markup=markup)
+def calculate_bmi(weight, height):
+    bmi = weight / (height/100)**2
+    return bmi
 
 
+@bot.message_handler(func=lambda message: True)
+def handle_keyboard(message):
+    if message.text == 'BMI':
+        bot.send_message(message.chat.id, 'Enter your weight(kg):')
+        bot.register_next_step_handler(message, ask_height)
+    elif message.text == 'About':
+        about(message)
 
+def ask_height(message):
+    weight = float(message.text)
+    bot.send_message(message.chat.id, 'Enter your height(cm):')
+    bot.register_next_step_handler(message, show_bmi, weight)
+
+def show_bmi(message, weight):
+    height = float(message.text)
+    bmi = calculate_bmi(weight, height)
+    bot.send_message(message.chat.id, f'Your BMI: {bmi:.2f}')
 
 bot.polling(none_stop=True, interval=1)
-
